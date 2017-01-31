80495fa44fb6d3877ad8c587cc32480b46925d61
NegativeRainbow <danishb@uw.edu>
2017-01-30 16:41:23 -0800

    Created Initial Activity and took screenshots

--- app/src/main/java/edu/washington/danishb/quizdroid/MainActivity.java
+++ app/src/main/java/edu/washington/danishb/quizdroid/MainActivity.java
@@ -2,12 +2,36 @@ package edu.washington.danishb.quizdroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.topicList);
+
+        String[] topics = new String[] {
+                "Math",
+                "Physics",
+                "Marvel Super Heroes",
+                "DC Superheroes"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, topics);
+        AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String topic = (String) listView.getItemAtPosition(i);
+            }
+        };
+        listView.setAdapter(adapter);
     }
 }
